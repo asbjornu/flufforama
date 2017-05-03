79e3cbf8638a05f93861681a035152c18f729cde
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2017-05-03 20:52:16 +0200

    Use JSome to colorize and pretty-print the JSON

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var fetch = require('node-fetch');
+var jsome = require('jsome');
 
 fetch('https://api.externalintegration.payex.com/psp/checkout')
     .then(res => {
         return res.json();
     }).then(json => {
-        console.log(json);
+        jsome(json);
     });
