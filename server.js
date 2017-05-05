ee5f0657e3f84e65d012682eff4212c8fb6b2f7b
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2017-05-05 22:13:50 +0200

    Added dotenv to load access token from ignored .env file and moved fetching of the Payment Session URL to the app bootstrapping

--- server.js
+++ server.js
@@ -1,30 +1,53 @@
-var fetch = require('node-fetch');
-var jsome = require('jsome');
-var express = require('express');
-var logger = require('morgan');
-var app = express();
-var template = require('pug').compileFile(__dirname + '/src/templates/index.pug')
+require('dotenv').config();
+
+const fetch = require('node-fetch');
+const jsome = require('jsome');
+const express = require('express');
+const logger = require('morgan');
+const app = express();
+const template = require('pug').compileFile(__dirname + '/src/templates/index.pug')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
 
-app.get('/', function(req, res, next) {
+app.get('/', (req, res, next) => {
     try {
-        fetch('https://api.externalintegration.payex.com/psp/checkout')
-            .then(res => {
-                return res.json();
-            }).then(json => {
-                var html = template({
-                    title: 'Home'
-                })
-                jsome(json);
-                res.send(html)
-            });
+        console.log(app.locals.paymentSessionUrl);
+
+        var html = template({
+            title: 'Home',
+        })
+        res.send(html)
     } catch (e) {
         next(e)
     }
 })
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, () => {
+    var accessToken = process.env.ACCESS_TOKEN;
+    if (!accessToken) {
+        console.error('No access token configured. Have you created an .env file with ACCESS_TOKEN=[token] in it?');
+    }
+    console.log(`Bootstrapping with Access Token: ${accessToken}.`)
     console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+
+    fetch('https://api.externalintegration.payex.com/psp/checkout', {
+        headers: {
+            authorization: 'Bearer ' + accessToken
+        }
+    }).then(res => {
+        return res.json();
+    }).then(json => {
+        jsome(json);
+        app.locals.paymentSessionUrl = json.paymentSession;
+    });
 })
+
+
+/*
+            var html = template({
+                title: 'Home',
+            })
+            jsome(json);
+            res.send(html)
+*/
